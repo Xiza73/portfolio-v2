cb8f76060774f91edc6ea2b63ab8eecc26769606
Xiza73 <m.fajardo.1098@gmail.com>
2026-03-18 00:29:11 -0500

    feature: cleaning data

--- src/data/social-links.ts
+++ src/data/social-links.ts
@@ -3,17 +3,17 @@ import { type SocialLink } from '@/types/social-link';
 export const socialLinks: SocialLink[] = [
   {
     label: 'GitHub',
-    href: 'https://github.com/xiza',
+    href: 'https://github.com/Xiza73',
     icon: 'github',
   },
   {
     label: 'LinkedIn',
-    href: 'https://linkedin.com/in/xiza',
+    href: 'https://www.linkedin.com/in/m-fajardo-as/',
     icon: 'linkedin',
   },
   {
     label: 'Email',
-    href: 'mailto:contact@xiza.dev',
+    href: 'mailto:m.fajardo.1098@gmail.com',
     icon: 'mail',
   },
 ];
